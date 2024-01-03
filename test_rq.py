517e5c6819035d22a0750f1e6cb8a4ff2cf369d5
Meltree <112416392+meltree@users.noreply.github.com>
2024-01-03 16:54:09 +0100

    Delete test_rq.py

--- test_rq.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-json = {"key": ';"b)?gAfO\9Ks]d*$i7NP,~ #X0h[53y&}(vItl|ECW.VuH§T_-!oeR+/64qz82wpmcZ%UY@G=DkBj1FL', "text": "Fick dich"}
-
-r = requests.post('http://212.87.214.127:8367/encrypt', json=json)
-
-print(r.text)
